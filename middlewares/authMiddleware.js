c99c83bff7a0f61541484025624369eabf800a7a
AnanyaM29 <116902312+AnanyaM29@users.noreply.github.com>
2023-10-13 01:22:08 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 export const requireSignIn = async(req,res,next) =>{
     try{
-        const decode = JWT.verify(req.headers.authorization,gluyxxesrj)
+        const decode = JWT.verify(req.headers.authorization,'gluyxxesrj')
         req.user=decode;
         next();
     }catch(error)
